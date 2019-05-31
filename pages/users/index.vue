9fb009ea07e9f30afeea4497d73892172df1d631
hamid shams <hrm.shams@gmail.com>
2019-05-31 17:13:37 +0430

    first commit

---
+++ pages/users/index.vue
@@ -0,0 +1,29 @@
+<template>
+  <section class="container">
+    <div>
+        {{title}}
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  data: function () {
+    return {
+      title: 'Hello World!'
+    }
+  },
+
+  // middleware : 'testmid',
+  
+  // async asyncData (context) {
+  //   return new Promise(function(resolve, reject){
+  //     setInterval(function(){
+  //       resolve({
+  //         title : "hello there"
+  //       })
+  //     }, 1000)
+  //   })
+  // }
+}
+</script>
---
+++ pages/users/one.vue
@@ -0,0 +1,16 @@
+<template>
+  <section class="container">
+    <div>
+        user one
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  transition: {
+    name: 'test',
+    mode: 'out-in'
+  }
+}
+</script>
