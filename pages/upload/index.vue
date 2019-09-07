651d9f28e955b280cfb057e8136212e567f23be0
hamid shams <hrm.shams@gmail.com>
2019-09-07 09:39:19 +0430

    upload front

---
+++ pages/upload/index.vue
@@ -0,0 +1,26 @@
+<template>
+     <div class = "container centralize">
+          <gap height="30"></gap>
+          <h4 class="mb-3">عکس مورد نظر خود را دراینجا آپلود کرده و منتظر نتایج بمانید</h4>
+          <gap height="30"></gap>
+          <form action="/" method="post" enctype="multipart/form-data" class="vertical">
+               <input type="file" name="photo" class="btn btn-light"/>
+               <gap height="10"></gap>
+               <input type="submit" name="Submit" value="آپلود عکس" class="btn btn-primary"/>
+          </form>
+     </div>
+</template>
+
+<script>
+export default {
+    layout : 'defaultLayout',
+     
+}
+</script>
+
+<style scoped>
+.vertical{
+     display: flex;
+     flex-direction: column;
+}
+</style>
