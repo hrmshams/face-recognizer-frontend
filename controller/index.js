f1a766897e7b8e4109c51f60fe2cddd219a423e3
hamid shams <hrm.shams@gmail.com>
2019-09-30 14:31:40 +0330

    finaly works!

--- controller/index.js
+++ controller/index.js
@@ -1,348 +1,437 @@
-import axios from 'axios'
-const qs = require('querystring')
+import axios from "axios"
+const qs = require("querystring")
 import store from "./../store/index"
+var FormData = require("form-data")
 
-axios.defaults.port = 8000;
+axios.defaults.port = 8000
 let baseUrl = "http://localhost:8000/"
 
 let endPoints = {
-     login : baseUrl + "api/credential/login",
-     register : baseUrl + "api/register",
-     getUser : baseUrl + "api/credential/getUser",
-     addPersonForCrawl : baseUrl + "api/credential/people/addPersonForCrowl",
-     crawlImages : baseUrl + "api/credential/people/crawlImages",
-     getPeople : baseUrl + 'api/credential/people/getPeople',
-     unpreprocessedPeopleGet : baseUrl + 'api/credential/preprocess/getPeoplePreprocess',
-     startPreprocess : baseUrl + 'api/credential/preprocess/start',
-     getAllUsers : baseUrl + 'api/credential/user/getAll',
-     deleteUser : baseUrl + 'api/credential/user/deleteUser',
-     promoteDemoteUser : baseUrl + 'api/credential/user/promoteDemote',
+  login: baseUrl + "api/credential/login",
+  register: baseUrl + "api/register",
+  getUser: baseUrl + "api/credential/getUser",
+  addPersonForCrawl: baseUrl + "api/credential/people/addPersonForCrowl",
+  crawlImages: baseUrl + "api/credential/people/crawlImages",
+  getPeople: baseUrl + "api/credential/people/getPeople",
+  unpreprocessedPeopleGet:
+    baseUrl + "api/credential/preprocess/getPeoplePreprocess",
+  startPreprocess: baseUrl + "api/credential/preprocess/start",
+  getAllUsers: baseUrl + "api/credential/user/getAll",
+  deleteUser: baseUrl + "api/credential/user/deleteUser",
+  promoteDemoteUser: baseUrl + "api/credential/user/promoteDemote",
 
-     getReadyPeople : baseUrl + 'api/credential/featurevector/getReadyPeople',
-     getInfo : baseUrl + 'api/credential/featurevector/getInfo',
-     startCreatingVector : baseUrl + 'api/credential/featurevector/create',
+  getReadyPeople: baseUrl + "api/credential/featurevector/getReadyPeople",
+  getInfo: baseUrl + "api/credential/featurevector/getInfo",
+  startCreatingVector: baseUrl + "api/credential/featurevector/create",
 
-     getTrainingInfo : baseUrl + 'api/credential/train/getInfo',
-     startTraining : baseUrl + 'api/credential/train/start',
+  getTrainingInfo: baseUrl + "api/credential/train/getInfo",
+  startTraining: baseUrl + "api/credential/train/start",
+
+  upload: baseUrl + "api/credential/facedetect/upload",
+  startComparingProcess: baseUrl + "api/credential/facedetect/start",
+  getComparingProcessInfo:
+    baseUrl + "api/credential/facedetect/getComparingProcessInfo"
 }
 
-function getToken(){
-     return store().getters.getToken
+function getToken() {
+  return store().getters.getToken
 }
 
-export function login(username, password, onSuccess, onFailure){
-     const headers = {
-          'Authorization': "Basic " + "YXBwbGljYXRpb246c2VjcmV0",
-          'Content-Type': 'application/x-www-form-urlencoded'
-     }
-
-     const data = {
-          grant_type : "password",
-          username : username,
-          password : password
-     }
-     console.log(endPoints.login)
-     axios({
-          method : 'POST',
-          url : endPoints.login,
-          headers : headers,
-          data : qs.stringify(data)
-
-          }).then(function(response){
-               onSuccess(response.data)
-
-          }).catch(function(error){
-               onFailure(error)
-     })
-}
-
-export function register(username, password, onSuccess, onFailure){
-     const data = {
-          username : username,
-          password : password
-     }
-
-     axios({
-          method : 'POST',
-          url : endPoints.register,
-          // headers : headers,
-          crossDomain:true,
-          data : data
-          }).then(function(response){
-               onSuccess(response.data)
-
-          }).catch(function(error){
-               console.log(error)
-               onFailure(error)
-     })
-}
-
-export function getUser(token, onSuccess, onFailure){
-     const headers = {
-          'Authorization': "Bearer " + token,
-          'Content-Type': 'application/x-www-form-urlencoded'
-     }
-
-     axios({
-          method : 'GET',
-          url : endPoints.getUser,
-          headers : headers,
-          crossDomain:true,
-          }).then(function(response){
-               onSuccess(response.data)
-
-          }).catch(function(error){
-               console.log(error)
-               onFailure(error)
-     })
-}
-
-export function addPersonForCrawl(name, onSuccess, onFailure){
-     const headers = {
-          'Authorization': "Bearer " + getToken(),
-     }
-     console.log(headers)
-     const data = {
-          name : name,
-     }
-
-     axios({
-          method : 'POST',
-          url : endPoints.addPersonForCrawl,
-          headers : headers,
-          data : data,
-          crossDomain:true,
-          }).then(function(response){
-               onSuccess(response.data)
-
-          }).catch(function(error){
-               console.log(error)
-               onFailure(error)
-     })
-}
-
-export function startCrawlingImages(onSuccess, onFailure){
-     const headers = {
-          'Authorization': "Bearer " + getToken(),
-     }
-     axios({
-          method : 'POST',
-          url : endPoints.crawlImages,
-          headers : headers,
-          crossDomain:true,
-          }).then(function(response){
-               onSuccess(response.data)
-
-          }).catch(function(error){
-               console.log(error)
-               onFailure(error)
-     })
-}
-
-export function getPeople(onSuccess, onFailure){
-     const headers = {
-          'Authorization': "Bearer " + getToken(),
-     }
-     axios({
-          method : 'GET',
-          url : endPoints.getPeople,
-          headers : headers,
-          crossDomain:true,
-          }).then(function(response){
-               onSuccess(response.data)
-
-          }).catch(function(error){
-               console.log(error)
-               onFailure(error)
-     })
-}
-
-
-export function getUnpreprocessedPeople(onSuccess, onFailure){
+export function login(username, password, onSuccess, onFailure) {
   const headers = {
-       'Authorization': "Bearer " + getToken(),
+    Authorization: "Basic " + "YXBwbGljYXRpb246c2VjcmV0",
+    "Content-Type": "application/x-www-form-urlencoded"
+  }
+
+  const data = {
+    grant_type: "password",
+    username: username,
+    password: password
+  }
+  console.log(endPoints.login)
+  axios({
+    method: "POST",
+    url: endPoints.login,
+    headers: headers,
+    data: qs.stringify(data)
+  })
+    .then(function(response) {
+      onSuccess(response.data)
+    })
+    .catch(function(error) {
+      onFailure(error)
+    })
+}
+
+export function register(username, password, onSuccess, onFailure) {
+  const data = {
+    username: username,
+    password: password
+  }
+
+  axios({
+    method: "POST",
+    url: endPoints.register,
+    // headers : headers,
+    crossDomain: true,
+    data: data
+  })
+    .then(function(response) {
+      onSuccess(response.data)
+    })
+    .catch(function(error) {
+      console.log(error)
+      onFailure(error)
+    })
+}
+
+export function getUser(token, onSuccess, onFailure) {
+  const headers = {
+    Authorization: "Bearer " + token,
+    "Content-Type": "application/x-www-form-urlencoded"
+  }
+
+  axios({
+    method: "GET",
+    url: endPoints.getUser,
+    headers: headers,
+    crossDomain: true
+  })
+    .then(function(response) {
+      onSuccess(response.data)
+    })
+    .catch(function(error) {
+      console.log(error)
+      onFailure(error)
+    })
+}
+
+export function addPersonForCrawl(name, onSuccess, onFailure) {
+  const headers = {
+    Authorization: "Bearer " + getToken()
+  }
+  console.log(headers)
+  const data = {
+    name: name
+  }
+
+  axios({
+    method: "POST",
+    url: endPoints.addPersonForCrawl,
+    headers: headers,
+    data: data,
+    crossDomain: true
+  })
+    .then(function(response) {
+      onSuccess(response.data)
+    })
+    .catch(function(error) {
+      console.log(error)
+      onFailure(error)
+    })
+}
+
+export function startCrawlingImages(onSuccess, onFailure) {
+  const headers = {
+    Authorization: "Bearer " + getToken()
   }
   axios({
-       method : 'GET',
-       url : endPoints.unpreprocessedPeopleGet,
-       headers : headers,
-       crossDomain:true,
-       }).then(function(response){
-            onSuccess(response.data)
-
-       }).catch(function(error){
-            console.log(error)
-            onFailure(error)
+    method: "POST",
+    url: endPoints.crawlImages,
+    headers: headers,
+    crossDomain: true
   })
+    .then(function(response) {
+      onSuccess(response.data)
+    })
+    .catch(function(error) {
+      console.log(error)
+      onFailure(error)
+    })
 }
 
-export function startPreprocess(onSuccess, onFailure){
+export function getPeople(onSuccess, onFailure) {
   const headers = {
-       'Authorization': "Bearer " + getToken(),
+    Authorization: "Bearer " + getToken()
   }
   axios({
-       method : 'POST',
-       url : endPoints.startPreprocess,
-       headers : headers,
-       crossDomain:true,
-       }).then(function(response){
-            onSuccess(response.data)
-
-       }).catch(function(error){
-            console.log(error)
-            onFailure(error)
+    method: "GET",
+    url: endPoints.getPeople,
+    headers: headers,
+    crossDomain: true
   })
+    .then(function(response) {
+      onSuccess(response.data)
+    })
+    .catch(function(error) {
+      console.log(error)
+      onFailure(error)
+    })
 }
 
-export function getAllUsers(onSuccess, onFailure){
-     const headers = {
-          'Authorization': "Bearer " + getToken(),
-     }
-     axios({
-          method : 'GET',
-          url : endPoints.getAllUsers,
-          headers : headers,
-          crossDomain:true,
-          }).then(function(response){
-               onSuccess(response.data)
-   
-          }).catch(function(error){
-               console.log(error)
-               onFailure(error)
-     })
+export function getUnpreprocessedPeople(onSuccess, onFailure) {
+  const headers = {
+    Authorization: "Bearer " + getToken()
+  }
+  axios({
+    method: "GET",
+    url: endPoints.unpreprocessedPeopleGet,
+    headers: headers,
+    crossDomain: true
+  })
+    .then(function(response) {
+      onSuccess(response.data)
+    })
+    .catch(function(error) {
+      console.log(error)
+      onFailure(error)
+    })
 }
 
-export function deleteUser(user_id, onSuccess, onFailure){
-     const headers = {
-          'Authorization': "Bearer " + getToken(),
-     }
-     const data = {
-          id : user_id
-     }
-     axios({
-          method : 'POST',
-          url : endPoints.deleteUser,
-          headers : headers,
-          data : data,
-          crossDomain:true,
-          }).then(function(response){
-               onSuccess(response.data)
-
-          }).catch(function(error){
-               console.log(error)
-               onFailure(error)
-     })
+export function startPreprocess(onSuccess, onFailure) {
+  const headers = {
+    Authorization: "Bearer " + getToken()
+  }
+  axios({
+    method: "POST",
+    url: endPoints.startPreprocess,
+    headers: headers,
+    crossDomain: true
+  })
+    .then(function(response) {
+      onSuccess(response.data)
+    })
+    .catch(function(error) {
+      console.log(error)
+      onFailure(error)
+    })
 }
 
-export function promoteDemoteUser(user_id, isPromote, onSuccess, onFailure){
-     const headers = {
-          'Authorization': "Bearer " + getToken(),
-     }
-     const data = {
-          id : user_id,
-          isPromote : isPromote
-     }
-     axios({
-          method : 'POST',
-          url : endPoints.promoteDemoteUser,
-          headers : headers,
-          data : data,
-          crossDomain:true,
-          }).then(function(response){
-               onSuccess(response.data)
-   
-          }).catch(function(error){
-               console.log(error)
-               onFailure(error)
-     })
+export function getAllUsers(onSuccess, onFailure) {
+  const headers = {
+    Authorization: "Bearer " + getToken()
+  }
+  axios({
+    method: "GET",
+    url: endPoints.getAllUsers,
+    headers: headers,
+    crossDomain: true
+  })
+    .then(function(response) {
+      onSuccess(response.data)
+    })
+    .catch(function(error) {
+      console.log(error)
+      onFailure(error)
+    })
 }
 
-export function getReadyPeople(onSuccess, onFailure){
-     const headers = {
-          'Authorization': "Bearer " + getToken(),
-     }
-     axios({
-          method : 'GET',
-          url : endPoints.getReadyPeople,
-          headers : headers,
-          crossDomain:true,
-          }).then(function(response){
-               onSuccess(response.data)
-   
-          }).catch(function(error){
-               console.log(error)
-               onFailure(error)
-     })
+export function deleteUser(user_id, onSuccess, onFailure) {
+  const headers = {
+    Authorization: "Bearer " + getToken()
+  }
+  const data = {
+    id: user_id
+  }
+  axios({
+    method: "POST",
+    url: endPoints.deleteUser,
+    headers: headers,
+    data: data,
+    crossDomain: true
+  })
+    .then(function(response) {
+      onSuccess(response.data)
+    })
+    .catch(function(error) {
+      console.log(error)
+      onFailure(error)
+    })
 }
 
-export function getVectorCreatingInfo(onSuccess, onFailure){
-     const headers = {
-          'Authorization': "Bearer " + getToken(),
-     }
-     axios({
-          method : 'GET',
-          url : endPoints.getInfo,
-          headers : headers,
-          crossDomain:true,
-          }).then(function(response){
-               onSuccess(response.data)
-   
-          }).catch(function(error){
-               console.log(error)
-               onFailure(error)
-     })
+export function promoteDemoteUser(user_id, isPromote, onSuccess, onFailure) {
+  const headers = {
+    Authorization: "Bearer " + getToken()
+  }
+  const data = {
+    id: user_id,
+    isPromote: isPromote
+  }
+  axios({
+    method: "POST",
+    url: endPoints.promoteDemoteUser,
+    headers: headers,
+    data: data,
+    crossDomain: true
+  })
+    .then(function(response) {
+      onSuccess(response.data)
+    })
+    .catch(function(error) {
+      console.log(error)
+      onFailure(error)
+    })
 }
 
-export function startCreatingVector(onSuccess, onFailure){
-     const headers = {
-          'Authorization': "Bearer " + getToken(),
-     }
-     axios({
-          method : 'POST',
-          url : endPoints.startCreatingVector,
-          headers : headers,
-          crossDomain:true,
-          }).then(function(response){
-               onSuccess(response.data)
-   
-          }).catch(function(error){
-               console.log(error)
-               onFailure(error)
-     })
+export function getReadyPeople(onSuccess, onFailure) {
+  const headers = {
+    Authorization: "Bearer " + getToken()
+  }
+  axios({
+    method: "GET",
+    url: endPoints.getReadyPeople,
+    headers: headers,
+    crossDomain: true
+  })
+    .then(function(response) {
+      onSuccess(response.data)
+    })
+    .catch(function(error) {
+      console.log(error)
+      onFailure(error)
+    })
 }
 
-export function startTraining(onSuccess, onFailure){
-     const headers = {
-          'Authorization': "Bearer " + getToken(),
-     }
-     axios({
-          method : 'POST',
-          url : endPoints.startTraining,
-          headers : headers,
-          crossDomain:true,
-          }).then(function(response){
-               onSuccess(response.data)
-   
-          }).catch(function(error){
-               console.log(error)
-               onFailure(error)
-     })
+export function getVectorCreatingInfo(onSuccess, onFailure) {
+  const headers = {
+    Authorization: "Bearer " + getToken()
+  }
+  axios({
+    method: "GET",
+    url: endPoints.getInfo,
+    headers: headers,
+    crossDomain: true
+  })
+    .then(function(response) {
+      onSuccess(response.data)
+    })
+    .catch(function(error) {
+      console.log(error)
+      onFailure(error)
+    })
 }
 
-export function getTrainingInfo(onSuccess, onFailure){
-     const headers = {
-          'Authorization': "Bearer " + getToken(),
-     }
-     axios({
-          method : 'GET',
-          url : endPoints.getTrainingInfo,
-          headers : headers,
-          crossDomain:true,
-          }).then(function(response){
-               onSuccess(response.data)
-   
-          }).catch(function(error){
-               console.log(error)
-               onFailure(error)
-     })
+export function startCreatingVector(onSuccess, onFailure) {
+  const headers = {
+    Authorization: "Bearer " + getToken()
+  }
+  axios({
+    method: "POST",
+    url: endPoints.startCreatingVector,
+    headers: headers,
+    crossDomain: true
+  })
+    .then(function(response) {
+      onSuccess(response.data)
+    })
+    .catch(function(error) {
+      console.log(error)
+      onFailure(error)
+    })
+}
+
+export function startTraining(onSuccess, onFailure) {
+  const headers = {
+    Authorization: "Bearer " + getToken()
+  }
+  axios({
+    method: "POST",
+    url: endPoints.startTraining,
+    headers: headers,
+    crossDomain: true
+  })
+    .then(function(response) {
+      onSuccess(response.data)
+    })
+    .catch(function(error) {
+      console.log(error)
+      onFailure(error)
+    })
+}
+
+export function getTrainingInfo(onSuccess, onFailure) {
+  const headers = {
+    Authorization: "Bearer " + getToken()
+  }
+  axios({
+    method: "GET",
+    url: endPoints.getTrainingInfo,
+    headers: headers,
+    crossDomain: true
+  })
+    .then(function(response) {
+      onSuccess(response.data)
+    })
+    .catch(function(error) {
+      console.log(error)
+      onFailure(error)
+    })
+}
+
+export function upload(file, onSuccess, onFailure) {
+  const headers = {
+    Authorization: "Bearer " + getToken(),
+    "Content-Type": "multipart/form-data"
+  }
+
+  let formData = new FormData()
+  formData.append("file", file)
+
+  // let data = {
+  //      file : formData
+  // }
+  axios({
+    method: "POST",
+    url: endPoints.upload,
+    headers: headers,
+    data: formData,
+    crossDomain: true
+  })
+    .then(function(response) {
+      onSuccess(response.data)
+    })
+    .catch(function(error) {
+      console.log(error)
+      onFailure(error)
+    })
+}
+
+export function startComparingProcess(onSuccess, onFailure) {
+  const headers = {
+    Authorization: "Bearer " + getToken()
+  }
+
+  axios({
+    method: "POST",
+    url: endPoints.startComparingProcess,
+    headers: headers,
+    crossDomain: true
+  })
+    .then(function(response) {
+      onSuccess(response.data)
+    })
+    .catch(function(error) {
+      console.log(error)
+      onFailure(error)
+    })
+}
+
+export function getComparingProcessInfo(onSuccess, onFailure) {
+  const headers = {
+    Authorization: "Bearer " + getToken()
+  }
+
+  axios({
+    method: "GET",
+    url: endPoints.getComparingProcessInfo,
+    headers: headers,
+    crossDomain: true
+  })
+    .then(function(response) {
+      onSuccess(response.data)
+    })
+    .catch(function(error) {
+      console.log(error)
+      onFailure(error)
+    })
 }
--- pages/upload/index.vue
+++ pages/upload/index.vue
@@ -1,38 +1,184 @@
 <template>
-     <div class = "container centralize">
-          <gap height="30"></gap>
-          <h4 class="mb-3">عکس مورد نظر خود را دراینجا آپلود کرده و منتظر نتایج بمانید</h4>
-          <gap height="30"></gap>
-          <form action="/" method="post" enctype="multipart/form-data" class="vertical">
-               <input type="file" name="photo" class="btn btn-light"/>
-               <gap height="10"></gap>
-               <input type="submit" name="Submit" value="آپلود عکس" class="btn btn-primary"/>
-          </form>
-     </div>
+  <div class="container centralize horizontal">
+    <div class="right-panel panel">
+      <lable class="mt-3 centralize">
+        عکس مورد نظر خود را دراینجا آپلود کرده و منتظر نتایج بمانید
+      </lable>
+
+      <gap height="30"></gap>
+      <form class="vertical">
+        <input
+          type="file"
+          name="photo"
+          class="btn btn-light"
+          ref="imageRef"
+          @change="setImage"
+          accept=".jpg"
+        />
+        <gap height="10"></gap>
+        <input
+          type="submit"
+          name="Submit"
+          value="شروع فرایند"
+          class="btn btn-primary"
+          v-on:click.stop.prevent="onSubmit"
+          :disabled="isUploading || isProcessing"
+        />
+        <gap height="15"></gap>
+        <flash-message transitionIn="animated swing"></flash-message>
+        <div class="line"></div>
+
+        <div class="result-container horizontal" v-show="isShowingResult()">
+          <div class="spinner-grow text-warning" role="status">
+            <span class="sr-only">Loading...</span>
+          </div>
+          <div>{{ result_text() }}</div>
+        </div>
+        <div class="result-container horizontal" v-show="done">
+          <label>عملیات با موفقیت تمام شد</label>
+          <label
+            >فرد پیش بینی شده {{ this.result.person }} با میزان اطمینان
+            {{ this.result.confidence }} می باشد</label
+          >
+        </div>
+      </form>
+    </div>
+
+    <div class="left-panel panel centralize">
+      <img v-if="url" :src="url" />
+    </div>
+  </div>
 </template>
 
 <script>
+import {
+  upload,
+  startComparingProcess,
+  getComparingProcessInfo
+} from "~/controller/index"
 export default {
-    layout : 'defaultLayout',
-    middleware : 'auth',
+  layout: "defaultLayout",
+  middleware: "auth",
+  data() {
+    return {
+      image: null,
+      url: null,
+      isUploading: false,
+      isProcessing: false,
+      done: false,
+      result_texts: ["درحال آپلود عکس", "درحال پردازش", "تمام شد!"],
+      result: {
+        person: "",
+        confidence: ""
+      }
+    }
+  },
+  methods: {
+    setImage() {
+      this.image = this.$refs.imageRef.files[0]
+      this.url = URL.createObjectURL(this.image)
+    },
+    onSubmit() {
+      if (this.image) {
+        this.isUploading = true
+        setTimeout(() => {
+          upload(
+            this.image,
+            res => {
+              console.log(res)
+              this.isUploading = false
+              this.isProcessing = true
 
-    // asyncData ({app, store, redirect}) {
+              startComparingProcess(
+                res => {
+                  setTimeout(this.refresh, 1000)
+                },
+                err => {
+                  console.log("error in running script:" + err)
+                  this.flash("مشکلی در اجرای اسکریپت پیش آمد", "warning")
+                }
+              )
+            },
+            err => {
+              console.log(err)
+            }
+          )
+        }, 2000)
+      } else {
+        this.flash("عکسی انتخاب نشده است!", "warning")
+      }
+    },
+    result_text() {
+      if (this.isUploading) return this.result_texts[0]
+      if (this.isProcessing) return this.result_texts[1]
+      return ""
+    },
+    isShowingResult() {
+      return this.isUploading || this.isProcessing
+    },
+    refresh() {
+      console.log("accsss")
+      getComparingProcessInfo(
+        res => {
+          console.log(res)
+          if (res.status === "processing") {
+            setTimeout(this.refresh, 1000)
+          } else if (res.status === "done") {
+            this.isProcessing = false
+            this.done = true
+            this.result = {
+              person: res.p_person,
+              confidence: res.confidence
+            }
+          }
+        },
+        err => {}
+      )
+    }
+  }
+  // asyncData ({app, store, redirect}) {
 
-    // const scope = app.$cookies.get('scope')
-    // if (scope){
-    //   if (!scope.includes('user')){
-    //     redirect('/login')
-    //   }
-    // }else {
-    //   redirect('/login')      
-    // }
-  
+  // const scope = app.$cookies.get('scope')
+  // if (scope){
+  //   if (!scope.includes('user')){
+  //     redirect('/login')
+  //   }
+  // }else {
+  //   redirect('/login')
+  // }
 }
 </script>
 
 <style scoped>
-.vertical{
-     display: flex;
-     flex-direction: column;
+.vertical {
+  display: flex;
+  flex-direction: column;
+}
+.horizontal {
+  display: flex;
+  flex-direction: row;
+}
+.left-panel {
+  width: 60%;
+}
+.right-panel {
+  width: 40%;
+}
+.panel {
+  margin: 15px;
+  padding: 20px;
+  height: 500px;
+  border-radius: 10px;
+  background-color: #fff;
+}
+.line {
+  height: 1px;
+  background-color: #e0e0e0;
+}
+.result-container {
+  padding: 10px;
+  /* justify-content: center; */
+  align-items: center;
+  color: #666666;
 }
 </style>
